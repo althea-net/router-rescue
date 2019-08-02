5a9a50ead64c26a41188d75453c51a7633921e75
Adam Soltys <asoltys@gmail.com>
2019-08-02 09:17:09 -0700

    add build step to script

--- release.sh
+++ release.sh
@@ -2,6 +2,7 @@
 
 # This script assumes you have router-rescue-signing-key.jks in the project root
 
+./gradlew build
 RELEASE_DIR=app/build/outputs/apk/release
 rm $RELEASE_DIR/app-release-unsigned-aligned.apk
 zipalign -v -p 4 $RELEASE_DIR/app-release-unsigned.apk $RELEASE_DIR/app-release-unsigned-aligned.apk
